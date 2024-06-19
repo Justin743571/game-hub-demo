65c4a3f0dbbdd0f0f606011af3e0a14e7e2433a9
王旭 <wxu743571@gmail.com>
2024-06-19 16:44:16 +0800

    添加GameCardSkeleton改善用户体验

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,23 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-
-  if (error) return <Text>{error.message}</Text>;
-  if (isLoading) return <Text>Loading...</Text>;
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
-    <SimpleGrid columns={{ sm:1, md:2, lg:4}} spacing={10} padding="10px">
-      {data?.results.map((game) => (
-        <GameCard game={game} key={game.id} />
-      ))}
-    </SimpleGrid>
+    <>
+      {error && <Text>{error.message}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} spacing={10} padding="10px">
+        {isLoading &&
+          skeletons.map((index) => <GameCardSkeleton key={index} />)}
+        {data?.results.map((game) => (
+          <GameCard game={game} key={game.id} />
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
 
