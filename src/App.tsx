497c04c2527c53657f08591b31f99dd5880509da
王旭 <wxu743571@gmail.com>
2024-06-19 14:21:07 +0800

    创建响应式布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Heading>Hello World!</Heading>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
